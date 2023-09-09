f1ae58cfcfc8970d17d78b6baf2da4daf2ae5490
Nweremizu <nweremizu.bruno@stu.cu.edu.ng>
2023-09-09 20:44:38 +0100

    Added some more data to the data dictionary

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 
 
-@app.route('/', methods=['GET'])
+@app.route('/api', methods=['GET'])
 def stage_one_request():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track').lower()
@@ -15,9 +15,11 @@ def stage_one_request():
     data = {
         'slack_name': slack_name,
         'current_day': datetime.now().strftime("%A"),
-        'track': track,
         'utc_time': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
-        'status_response': 200,
+        'track': track,
+        'github_file_url': 'https://github.com/Nweremizu/Backend-Stage-One/blob/main/app.py',
+        'github_repo_url': 'https://github.com/Nweremizu/Backend-Stage-One',
+        'status_response': 200
     }
 
     return jsonify(data)
